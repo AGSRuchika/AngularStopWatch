e4f2a862c4fdeb4ae3dcd27cc4ad925d5c840718
AGS Ruchika <agsruchika@gmail.com>
2023-10-13 00:10:34 +0530

    Stop Watch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StopwatchComponent } from './stopwatch/stopwatch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/stopwatch', pathMatch: 'full'},
+  {path:'stopwatch', component:StopwatchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stopwatch',
+  templateUrl: './stopwatch.component.html',
+  styleUrls: ['./stopwatch.component.scss'],
+})
+export class StopwatchComponent {
+  minute = 59;
+  hour = 1;
+  second = 50;
+  running = 'false';
+  timer: any;
+
+  backgroundImageUrls: string[] = [
+    '../../assets/1.webp',
+    '../../assets/2.jpg',
+    '../../assets/4.jpg',
+    '../../assets/5.jpg',
+    '../../assets/6.jpg',
+    '../../assets/7.jpg',
+  ];
+  currentImageIndex = 0;
+
+  startimer() {
+    if (this.running) {
+      this.second++;
+      if (this.second === 60) {
+        this.second = 0;
+        this.minute++;
+        if (this.minute === 60) {
+          this.minute = 0;
+          this.hour++;
+        }
+      }
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.backgroundImageUrls.length;
+
+      this.timer=setTimeout(() => this.startimer(), 1000);
+    }
+  }
+
+  start() {
+    this.running = 'true';
+    this.startimer();
+    console.log(this.timer);
+  }
+
+  stop() {
+    this.running = 'false';
+    clearTimeout(this.timer);
+    console.log(this.hour, this.minute, this.second);
+  }
+
+  reset() {
+    this.minute = 0;
+    this.hour = 0;
+    this.second = 0;
+  }
+
+  getCurrentImageUrl() {
+    return this.backgroundImageUrls[this.currentImageIndex];
+  }
+}
